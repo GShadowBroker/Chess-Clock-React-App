c050385ff614f04c70fa562aae78b48417033e0b
Gledyson <gledysonferreira@gmail.com>
2020-05-19 03:31:27 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,150 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Clock from './Clock.js'
+import './Navigation.css'
+import { FaBars, FaVolumeUp, FaVolumeMute, FaStopwatch } from 'react-icons/fa'
+import { AiFillFire, AiFillThunderbolt } from 'react-icons/ai'
+
+
+// function Drawer(props) {
+//   return (
+//     <div className="drawer">
+//       <ul>
+//         <li className="drawer-item">Bullet</li>
+//         <li className="drawer-item">Blitz</li>
+//         <li className="drawer-item">Blitz</li>
+//         <li className="drawer-item">Rapid</li>
+//         <li className="drawer-item">Rapid</li>
+//       </ul>
+//     </div>
+//   )
+// }
 
 function App() {
+
+  let [ timeControl, setTimeControl ] = useState(300)
+  let [ increment, setIncrement ] = useState(3)
+  let [ muted, setMuted ] = useState(false)
+  let [ timeControlsList ] = useState([
+    { id: 0, title: 'Bullet', seconds: 60, increment: 0, icon: <AiFillFire /> },
+    { id: 1, title: 'Blitz', seconds: 180, increment: 2, icon: <AiFillThunderbolt /> },
+    { id: 2, title: 'Blitz', seconds: 300, increment: 3, icon: <AiFillThunderbolt /> },
+    { id: 3, title: 'Rapid', seconds: 900, increment: 10, icon: <FaStopwatch /> },
+    { id: 4, title: 'Rapid', seconds: 1500, increment: 10, icon: <FaStopwatch /> }
+  ])
+  let [ selectedTimeControl, setSelectedTimeControl ] = useState(2)
+  let [ isDrawerOpen, setIsDrawerOpen ] = useState(false)
+
+  useEffect(() => {
+    for (let elem of document.getElementsByClassName('time-control')) {
+      if (Number(elem.id) === Number(selectedTimeControl)) {
+        elem.classList.add('active')
+      } else {
+        elem.classList.remove('active')
+      }
+    }
+    for (let elem of document.getElementsByClassName('drawer-item')) {
+      if (Number(elem.id) === Number(selectedTimeControl)) {
+        elem.classList.add('active')
+      } else {
+        elem.classList.remove('active')
+      }
+    }
+  })
+
+  const openDrawer = () => {
+    document.querySelector('.drawer').style.width = "250px"
+    document.getElementById('main').style.marginLeft = "250px"
+    document.querySelector('.nav').style.transform = "translate(250px)"
+    setIsDrawerOpen(true)
+  }
+
+  const closeDrawer = () => {
+    document.querySelector('.drawer').style.width = "0"
+    document.getElementById('main').style.marginLeft = "0"
+    document.querySelector('.nav').style.transform = "translate(0px)"
+    setIsDrawerOpen(false)
+  }
+
+  const setTimeControlConfig = (e) => {
+    let index = e.currentTarget.id
+    setTimeControl(timeControlsList[index].seconds)
+    setIncrement(timeControlsList[index].increment)
+    setSelectedTimeControl(index)
+  }
+
+  const timeControls = timeControlsList.map(item => (
+    <li
+        className={ selectedTimeControl === item.id
+            ? 'time-control active'
+            : 'time-control' }
+        id={ item.id }
+        key={ item.id }
+        onClick={ setTimeControlConfig }
+      >
+        <span className="time-control-icon">{ item.icon }</span><span className="time-control-title">{ item.title }</span>
+        <div className="time-control-timer">{ item.seconds / 60 }+{ item.increment }</div>
+      </li>
+  ))
+
+  const timeControlsDrawer = timeControlsList.map(item => (
+    <li
+        className={ selectedTimeControl === item.id
+            ? 'drawer-item active'
+            : 'drawer-item' }
+        key={ item.id }
+        onClick={ setTimeControlConfig }
+        id={ item.id }
+      >
+        <div className="drawer-icon">
+          { item.icon }
+        </div>
+        <div className="drawer-text">
+          <span className="drawer-item-title">{ item.title }</span>
+        <div className="drawer-item-timer">{ item.seconds / 60 }+{ item.increment }</div>
+        </div>
+      </li>
+  ))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div id="main">
+        <div className="drawer">
+          <span id="close-drawer-button" onClick={ closeDrawer }>&times;</span>
+          <ul>
+            { timeControlsDrawer }
+          </ul>
+        </div>
+        
+        <div className="nav">
+          <div className="nav-title">
+            <FaBars className="drawer-button" onClick={ isDrawerOpen ? closeDrawer : openDrawer } />
+            <span className="chess-title">CHESS<strong className="clock-title">CLOCK</strong></span>
+          </div>
+          <div className="nav-menu">
+            <ul>
+              { timeControls }
+            </ul>
+          </div>
+        </div>
+        <Clock timeControl={ timeControl } increment={ increment } muted={ muted }/>
+        <footer>
+          <div className="copyright">&copy; { new Date().getFullYear() }</div>
+          <div className="press-space">
+            <p>Press <span style={{color: "#d35400"}}>SPACE</span> to pass turn</p>
+          </div>
+          <div className="mute-button">
+            { 
+              muted
+              ? <FaVolumeMute onClick={ () => setMuted(!muted) } />
+              : <FaVolumeUp onClick={ () => setMuted(!muted) } />
+            }
+          </div>
+        </footer>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Clock.js
@@ -0,0 +1,243 @@
+import React, { Component } from 'react'
+import './Clock.css'
+import moment from 'moment'
+import { BsFillPauseFill, BsFillPlayFill, BsFillSkipBackwardFill } from 'react-icons/bs'
+
+class Clock extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            gameStarted: false,
+            gameFinished: false,
+            paused: false,
+            blackTimer: this.props.timeControl,
+            whiteTimer: this.props.timeControl,
+            increment: this.props.increment,
+            toPlay: 'white',
+            turn: 1,
+            muted: this.props.muted
+        }
+        this.tickBlackClock = this.tickBlackClock.bind(this)
+        this.tickWhiteClock = this.tickWhiteClock.bind(this)
+        this.togglePause = this.togglePause.bind(this)
+        this.passTurn = this.passTurn.bind(this)
+        this.resetTimer = this.resetTimer.bind(this)
+        this.runWhiteTimer = null
+        this.runBlackTimer = null
+        this.passTurnAudio = new Audio('http://192.168.0.104:3000/button.wav')
+        this.playButtonAudio = this.playButtonAudio.bind(this)
+        this.timeoutAudio = new Audio('http://192.168.0.104:3000/timeout.wav')
+        this.playTimeoutAudio = this.playTimeoutAudio.bind(this)
+    }
+
+    componentDidMount() {
+        document.addEventListener('keyup', (e) => {
+            e.preventDefault()
+            if (e.keyCode === 32 && !this.state.gameFinished) {
+                this.passTurn()
+            }
+        })
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('keyup', (e) => {
+            e.preventDefault()
+            if (e.keyCode === 32 && !this.state.gameFinished) {
+                this.passTurn()
+            }
+        })
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.timeControl !== this.props.timeControl) {
+            this.setState({
+                whiteTimer: this.props.timeControl,
+                blackTimer: this.props.increment
+            })
+            this.resetTimer()
+        }
+        if (prevProps.muted !== this.props.muted) {
+            this.setState({
+                muted: this.props.muted
+            })
+        }
+    }
+
+    resetTimer() {
+        clearInterval(this.runBlackTimer)
+        clearInterval(this.runWhiteTimer)
+        this.setState({
+            gameStarted: false,
+            gameFinished: false,
+            whiteTimer: this.props.timeControl,
+            blackTimer: this.props.timeControl,
+            increment: this.props.increment,
+            muted: this.props.muted,
+            toPlay: 'white',
+            turn: 1,
+            paused: false
+        })
+    }
+
+    playButtonAudio() {
+        if (this.state.muted) return
+        this.passTurnAudio.play()
+    }
+    playTimeoutAudio() {
+        if (this.state.muted) return
+        this.timeoutAudio.play()
+    }
+
+    tickBlackClock() {
+        if (this.state.blackTimer > 0) {
+            this.setState((state) => ({
+                blackTimer: state.blackTimer - 1
+            }))
+        } else {
+            clearInterval(this.runWhiteTimer)
+            clearInterval(this.runBlackTimer)
+            this.setState({
+                gameFinished: true
+            })
+            if (!this.state.muted) this.playTimeoutAudio()
+        }
+    }
+
+    tickWhiteClock() {
+        if (this.state.whiteTimer > 0) {
+            this.setState((state) => ({
+                whiteTimer: state.whiteTimer - 1
+            }))
+        } else {
+            clearInterval(this.runWhiteTimer)
+            clearInterval(this.runBlackTimer)
+            this.setState({
+                gameFinished: true
+            })
+            if (!this.state.muted) this.playTimeoutAudio()
+        }
+    }
+
+    togglePause() {
+        if (!this.state.gameStarted) return
+
+        if (!this.state.paused) {
+            clearInterval(this.runWhiteTimer)
+            clearInterval(this.runBlackTimer)
+        } else {
+            this.state.toPlay === 'white'
+                ? this.runWhiteTimer = setInterval(this.tickWhiteClock, 1000)
+                : this.runBlackTimer = setInterval(this.tickBlackClock, 1000)
+        }
+
+        this.setState(state => ({
+            paused: !state.paused
+        }))
+    }
+
+    passTurn() {
+        const { gameStarted, toPlay } = this.state
+
+        if (!gameStarted) {
+            this.setState({
+                gameStarted: true,
+                toPlay: 'black',
+                paused: false
+            })
+
+            this.runBlackTimer = setInterval(this.tickBlackClock, 1000)
+            this.playButtonAudio()
+
+        } else if (toPlay === 'white') {
+            this.setState(state => ({
+                toPlay: 'black',
+                whiteTimer: state.whiteTimer + state.increment,
+                paused: false
+            }))
+
+            this.runBlackTimer = setInterval(this.tickBlackClock, 1000)
+            clearInterval(this.runWhiteTimer)
+            this.playButtonAudio()
+
+        } else {
+            this.setState(state => ({
+                toPlay: 'white',
+                blackTimer: state.blackTimer + state.increment,
+                turn: state.turn + 1,
+                paused: false
+            }))
+
+            this.runWhiteTimer = setInterval(this.tickWhiteClock, 1000)
+            clearInterval(this.runBlackTimer)
+            this.playButtonAudio()
+        }
+    }
+
+    render() {
+
+        let rightSwitchClass = "right-switch"
+        let leftSwitchClass = "left-switch"
+
+        if (this.state.toPlay === 'white') {
+            rightSwitchClass += " right-switch-pressed"
+        } else {
+            leftSwitchClass += " left-switch-pressed"
+        }   
+
+        return (
+            <div className="clock" key={ this.props.timeControl }>
+                <div className="clock-body">
+                    <div className="white-player-clock">
+                        <h1
+                            style={ this.state.whiteTimer < 10 ? { color: '#e74c3c' } : {}}
+                        >{ moment.utc(this.state.whiteTimer * 1000).format('mm.ss') }</h1>
+                    </div>
+                    <div className="black-player-clock">
+                        <h1
+                            style={ this.state.blackTimer < 10 ? { color: '#e74c3c' } : {}}
+                        >{ moment.utc(this.state.blackTimer * 1000).format('mm.ss') }</h1>
+                    </div>
+                    <div className="controls">
+                        <button
+                            className="pause-button"
+                            onClick={ this.togglePause }
+                            disabled={ this.state.gameFinished || !this.state.gameStarted }
+                        >
+                            {
+                                this.state.paused
+                                ? <span><BsFillPlayFill/> Unpause</span>
+                                : <span><BsFillPauseFill/> Pause</span>
+                            }
+                        </button>
+                        <button
+                            className="to-play-button"
+                            onClick={ this.passTurn }
+                            disabled={ this.state.gameFinished }
+                        >
+                            Turn { this.state.turn }:
+                            { this.state.toPlay === 'white' ? ' White To Play' : ' Black To Play' }
+                        </button>
+                        <button
+                            className="reset-button"
+                            onClick={ this.resetTimer }
+                        >
+                            <span>Reset<BsFillSkipBackwardFill/></span>
+                        </button>
+                        
+                    </div>
+                    <div className="roof"/>
+                    <div className={ leftSwitchClass }/>
+                    <div className={ rightSwitchClass }/>
+                </div>
+            </div>
+        )
+    }
+}
+
+Clock.defaultProps = {
+    timeControl: 300,
+    increment: 3,
+    muted: false
+}
+
+export default Clock
